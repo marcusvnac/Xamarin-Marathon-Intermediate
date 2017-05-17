3278ab9c8301bf2ef215c6c699bc9199c60dc0db
marcusvnac <marcusvnac@gmail.com>
2017-05-17 15:52:01 -0300

    Exercices 04-05

--- Lab03/Lab03/Lab03/MainPage.xaml.cs
+++ Lab03/Lab03/Lab03/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lab03.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,12 @@ namespace Lab03
         public MainPage()
         {
             InitializeComponent();
+
+            btnCarregar.Clicked += async (sender, e) =>
+            {
+                var tags = await Api.GetTagsAsync();
+                lvwTags.ItemsSource = tags;
+            };
         }
     }
 }
