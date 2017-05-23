981789cd7673ee7c0593845f507eff3d36be1f19
marcusvnac <marcusvnac@gmail.com>
2017-05-23 17:54:49 -0300

    Adding push notifications do Android project

--- Lab05/Lab05/Lab05.Android/Resources/Resource.Designer.cs
+++ Lab05/Lab05/Lab05.Android/Resources/Resource.Designer.cs
@@ -26,6 +26,7 @@ namespace Lab05.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Gcm.Client.Resource.String.library_name = global::Lab05.Droid.Resource.String.library_name;
 			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::Lab05.Droid.Resource.Attribute.actionBarSize;
 		}
 		
@@ -3711,6 +3712,9 @@ namespace Lab05.Droid
 			
 			// aapt resource value: 0x7f090038
 			public const int character_counter_pattern = 2131296312;
+			
+			// aapt resource value: 0x7f09003e
+			public const int library_name = 2131296318;
 			
 			// aapt resource value: 0x7f090000
 			public const int mr_button_content_description = 2131296256;
